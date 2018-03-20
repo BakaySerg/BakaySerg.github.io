9cc3248e610702cf553e761f8d10474958366aa9
Serg Bakay <bakay.sergey28@gmail.com>
2018-03-20 18:18:17 +0200

    ++

--- medmgmu/js/main.js
+++ medmgmu/js/main.js
@@ -11,43 +11,73 @@ $(document).ready(function(){
     });
 
     //slider
-    $('.slick-slider').slick({
+    $('.photo-slider').slick({
       responsive:true,
-      infinite: true,
+      // infinite: false,
       dots: false,
-      speed:500,
       arrows: true,
-      prevArrow: $('.prev'),
-      nextArrow: $('.next'),
-      slidesToShow: 3,
+      prevArrow: '<i class="slick-arrow slick-prev"><svg width="40" height="40"><path d="M23.5 11.5 L15.5 19.5 L23.5 27.5" stroke-linejoin="round" stroke-linecap="round" style="fill:none; stroke:#ce202e; stroke-width:3px"></path></svg></i>',
+      nextArrow: '<i class="slick-arrow slick-next"><svg width="40" height="40"><path d="M16.5 11.5 L24.5 19.5 L16.5 27.5" stroke-linejoin="round" stroke-linecap="round" style="fill:none; stroke:#ce202e; stroke-width:3px"></path></svg></i>',
+      slidesToShow: 4,
       slidesToScroll: 1,
       responsive: [{
-        breakpoint: 800,
+        breakpoint: 900,
         settings: {
-          draggable:false,
-          slidesToShow: 2
+          slidesToShow: 3
         }        
       },{
         breakpoint: 600,
         settings: {
-          slidesToShow: 1,
-          centerMode:true,
-          arrows: false,
-          draggable:true,
-          centerPadding:'80px'}
+          slidesToShow: 2,
+          draggable:true}
         },{
-        breakpoint: 480,
+        breakpoint: 420,
         settings: {
           slidesToShow: 1,
           centerMode:true,
           arrows: false,
           draggable:true,
-          centerPadding:'50px'
+          centerPadding:'30px'
         }
       }]
     });
 
-    // Попап-маленький
+    // Галерея
+    if ($('.photo-gallery').length){
+      $('.photo-gallery').each(function() {
+        $(this).magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Загрузка...',
+            mainClass: 'mfp-zoom-in',
+            removalDelay: 160,
+            fixedContentPos: true,
+            fixedBgPos: true,
+            autoFocusLast:true,
+            midClick: true,          
+            gallery: {
+              enabled: true,
+              tCounter: '%curr% из %total%',
+              navigateByImgClick: true
+            },
+            image: {
+              tError: '<a href="%url%">Изображение #%curr%</a> не загружается.',
+              titleSrc: function(item) {
+                return item.el.find('span').text();
+              }
+            },
+            callbacks: {
+              buildControls: function() {
+                if(this.arrowLeft) {
+                  this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
+                }
+              }
+            }
+        });
+      });      
+    };
+
+    // Попапы -обратной связи
     $('.js-popup').magnificPopup({
        type: 'inline',
        removalDelay: 300,
