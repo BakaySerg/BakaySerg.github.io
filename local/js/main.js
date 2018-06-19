725f61164da2b3e83024758a2f1cd5349f1f6e8d
Serg Bakay <bakay.sergey28@gmail.com>
2018-06-20 00:57:52 +0300

    icon

--- local/js/main.js
+++ local/js/main.js
@@ -100,6 +100,25 @@ $(document).ready(function(){
       });
     };
 
+    // var show = true;
+    // var countbox = ".counts";
+    // $(window).on("scroll load resize", function() {
+    //   if (!show)
+    //     return true;
+    //   var w_top = $(window).scrollTop();
+    //   var e_top = $(countbox).offset().top;
+    //   var w_height = $(window).height();
+    //   var d_height = $(document).height();
+    //   var e_height = $(countbox).outerHeight();
+    //   if (w_top + 750 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+    //     $(".spincrement").spincrement({
+    //      thousandSeparator: " ",
+    //      duration: 3000
+    //   });
+    //     show = false;
+    //   }
+    //   });
+
     //mask
     $("[type=tel]").mask("+7 (999) 999-9999");
      
